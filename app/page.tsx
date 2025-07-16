3abcb8fb19563cab71953f8ab59585a08f33c320
JapleenKaur3 <jkaur4_be22@thapar.edu>
2025-07-16 17:31:36 +0530

    Add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { redirect } from 'next/navigation';
+// app/page.tsx
+import LandingPage from "./(landing)/page";
 
 export default function Home() {
-  redirect('/sign-in');
+  return <LandingPage />;
 }
